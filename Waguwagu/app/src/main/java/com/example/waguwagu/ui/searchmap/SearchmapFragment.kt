1166e571e0bdaadaa3d281409e1e109b96da2f07
HongGunhwa <dundae@ajou.ac.kr>
2022-05-05 22:53:26 +0900

    지도검색 Googlemap 띄우기

--- Waguwagu/app/src/main/java/com/example/waguwagu/ui/searchmap/SearchmapFragment.kt
+++ Waguwagu/app/src/main/java/com/example/waguwagu/ui/searchmap/SearchmapFragment.kt
@@ -1,11 +1,19 @@
 package com.example.waguwagu.ui.searchmap
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.waguwagu.MainActivity
 import com.example.waguwagu.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,44 +25,67 @@ private const val ARG_PARAM2 = "param2"
  * Use the [SearchmapFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SearchmapFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class SearchmapFragment : Fragment(), OnMapReadyCallback {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    lateinit var mContext: Context
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        if (context is MainActivity) {
+            mContext = context
         }
     }
+    private lateinit var mView: MapView
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_searchmap, container, false)
+        var rootView = inflater.inflate(R.layout.fragment_searchmap, container, false)
+        mView = rootView.findViewById(R.id.mapView)
+        mView.onCreate(savedInstanceState)
+        mView.getMapAsync(this)
+        return rootView
+    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        // 주소:임시로 아주대학교
+        val sydney = LatLng(37.282753,127.044999)
+        googleMap.addMarker(
+            MarkerOptions().
+            position(sydney)
+                .title("TestMaker")
+        )
+        //지도 초기 확대.
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15.7f))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchmapFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchmapFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onStart() {
+        super.onStart()
+        mView.onStart()
     }
+    override fun onStop() {
+        super.onStop()
+        mView.onStop()
+    }
+    override fun onResume() {
+        super.onResume()
+        mView.onResume()
+    }
+    override fun onPause() {
+        super.onPause()
+        mView.onPause()
+    }
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mView.onLowMemory()
+    }
+    override fun onDestroy() {
+        mView.onDestroy()
+        super.onDestroy()
+    }
+
 }
