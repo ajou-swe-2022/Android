e1973c36d420277f7e0ac069db3fa60aced0f948
HongGunhwa <dundae@ajou.ac.kr>
2022-05-07 10:08:49 +0900

    지도검색 마커 클릭시 카드view 생성

--- Waguwagu/app/src/main/java/com/example/waguwagu/ui/searchmap/SearchmapFragment.kt
+++ Waguwagu/app/src/main/java/com/example/waguwagu/ui/searchmap/SearchmapFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.core.view.isVisible
 import com.example.waguwagu.MainActivity
 import com.example.waguwagu.R
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -13,6 +16,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 // TODO: Rename parameter arguments, choose names that match
@@ -25,18 +29,22 @@ private const val ARG_PARAM2 = "param2"
  * Use the [SearchmapFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SearchmapFragment : Fragment(), OnMapReadyCallback {
+class SearchmapFragment : Fragment(), OnMapReadyCallback,GoogleMap.OnMarkerClickListener {
 
     lateinit var mContext: Context
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
+
         if (context is MainActivity) {
             mContext = context
         }
+
+
     }
     private lateinit var mView: MapView
+    private lateinit var cardview: CardView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,22 +52,34 @@ class SearchmapFragment : Fragment(), OnMapReadyCallback {
         savedInstanceState: Bundle?
     ): View? {
         var rootView = inflater.inflate(R.layout.fragment_searchmap, container, false)
+        cardview=rootView.findViewById(R.id.card_view);
         mView = rootView.findViewById(R.id.mapView)
         mView.onCreate(savedInstanceState)
         mView.getMapAsync(this)
         return rootView
     }
     override fun onMapReady(googleMap: GoogleMap) {
+        // 초기 지도가 생성될 때 작동되는 함수.
         // 주소:임시로 아주대학교
         val sydney = LatLng(37.282753,127.044999)
         googleMap.addMarker(
-            MarkerOptions().
-            position(sydney)
-                .title("TestMaker")
+            MarkerOptions()
+                .position(sydney)
+                .title("내 위치")
+                .snippet("아주대")
+
         )
         //지도 초기 확대.
         googleMap.moveCamera(CameraUpdateFactory.zoomTo(15.7f))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        googleMap.setOnMarkerClickListener (this);
+    }
+
+    override fun onMarkerClick(p0: Marker) : Boolean {
+        Toast.makeText(mContext, "클릭시 정보생성 테스트.", Toast.LENGTH_SHORT).show()
+        cardview.visibility=View.VISIBLE;
+
+        return false;
 
     }
 
