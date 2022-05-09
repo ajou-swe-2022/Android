7ef2140a59db75cd981dc338e7b4facd3eb016f0
Park_haein <qlsil@naver.com>
2022-05-09 19:10:31 +0900

    주문목록 탭 구현

---
+++ Waguwagu/app/src/main/java/com/example/waguwagu/OrderlistAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.waguwagu
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.waguwagu.databinding.OrderListItemBinding
+import com.example.waguwagu.model.data.SearchData
+
+class OrderlistAdapter (val data:List<SearchData>, val date_reserved : List<String>) : RecyclerView.Adapter<OrderlistAdapter.SearchViewHolder>() {
+
+    inner class SearchViewHolder(private val binding : OrderListItemBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(searchData: SearchData, date: String){
+            binding.restName.text = searchData.restname
+            binding.restTag.text = searchData.resttag
+            binding.restAdmit.text = searchData.restadmit.toString()
+            binding.restSeatNum.text = searchData.restseatnum.toString()
+            binding.reserveTime.text = searchData.reservetime.toString()
+            binding.date.text = date
+        }
+    }
+
+    //상속받으면 자동 생성
+    //ViewHolder에 쓰일 Layout을 inflate하는 함수
+    //ViewGroup의 context를 사용하여 특정 화면에서 구현할 수 있도록 함
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        val binding = OrderListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return SearchViewHolder(binding)
+    }
+
+    //상속받으면 자동 생성
+    override fun getItemCount(): Int = data.size
+
+    //상속받으면 자동 생성
+    //ViewHolder에서 데이터 묶는 함수가 실행되는 곳
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        holder.bind(data[position], date_reserved[position])
+    }
+}
--- Waguwagu/app/src/main/java/com/example/waguwagu/ui/orderlist/OrderlistFragment.kt
+++ Waguwagu/app/src/main/java/com/example/waguwagu/ui/orderlist/OrderlistFragment.kt
@@ -5,18 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.waguwagu.OrderlistAdapter
 import com.example.waguwagu.R
+import com.example.waguwagu.databinding.FragmentOrderlistBinding
+import com.example.waguwagu.model.data.SearchData
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [OrderlistFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class OrderlistFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -24,37 +18,32 @@ class OrderlistFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var binding = FragmentOrderlistBinding.inflate(inflater, container, false)
+        val datas = mutableListOf<SearchData>()
+        val dates = mutableListOf<String>()
+
+        datas.apply {
+            add(SearchData("McDonalds", "Hamburger", 3, 12, 30))
+            add(SearchData("롯데리아", "Hamburger", 3, 12, 30))
+            add(SearchData("본죽", "한식", 3, 12, 30))
+            add(SearchData("푸라닭", "한식", 3, 12, 30))
+            add(SearchData("팔달관매점", "호텔", 3, 12, 30))
         }
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_orderlist, container, false)
-    }
+        dates.apply {
+            add("2022/01/12")
+            add("2022/02/23")
+            add("2022/02/28")
+            add("2022/03/03")
+            add("2022/05/09")
+        }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OrderlistFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OrderlistFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+        binding.orderRecyclerview.adapter = OrderlistAdapter(datas, dates)
+
+        return binding.root
     }
 }
