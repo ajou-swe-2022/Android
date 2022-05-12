af17aae608b00b6efc438b5ccf0c1d19e596b7a3
HongGunhwa <dundae@ajou.ac.kr>
2022-05-13 02:33:19 +0900

    Map viewbinding으로 변경,임시 data 적용

--- Waguwagu/app/src/main/java/com/example/waguwagu/ui/searchmap/SearchmapFragment.kt
+++ Waguwagu/app/src/main/java/com/example/waguwagu/ui/searchmap/SearchmapFragment.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,16 +15,20 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.example.waguwagu.databinding.FragmentSearchmapBinding
 import androidx.core.content.ContextCompat.getDrawable
 
 
 import com.example.waguwagu.MainActivity
 import com.example.waguwagu.R
+import com.example.waguwagu.SearchAdapter
+import com.example.waguwagu.model.data.SearchData
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.*
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -51,20 +56,31 @@ class SearchmapFragment : Fragment(), OnMapReadyCallback,GoogleMap.OnMarkerClick
 
 
     }
+
     private lateinit var mView: MapView
     private lateinit var cardview: CardView
     private lateinit var rootView:View
+    val datas = mutableListOf<SearchData>()
+    lateinit var binding:FragmentSearchmapBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        rootView = inflater.inflate(R.layout.fragment_searchmap, container, false)
-        cardview=rootView.findViewById(R.id.card_view);
-        mView = rootView.findViewById(R.id.mapView)
-        mView.onCreate(savedInstanceState)
-        mView.getMapAsync(this)
-        return rootView
+        datas.apply {
+            add(SearchData("McDonalds", "Hamburger", 3, 12, 30))
+            add(SearchData("롯데리아", "Hamburger", 3, 12, 30))
+            add(SearchData("본죽", "한식", 3, 12, 30))
+            add(SearchData("푸라닭", "한식", 3, 12, 30))
+            add(SearchData("팔달관매점", "호텔", 3, 12, 30))
+        }
+        binding=FragmentSearchmapBinding.inflate(inflater,container,false)
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.getMapAsync(this)
+        rootView=binding.getRoot()
+        System.out.println(datas)
+        return rootView;
+
     }
     override fun onMapReady(googleMap: GoogleMap) {
         // 초기 지도가 생성될 때 작동되는 함수.
@@ -78,30 +94,31 @@ class SearchmapFragment : Fragment(), OnMapReadyCallback,GoogleMap.OnMarkerClick
                 .snippet("예시용 내위치")
 
         )?.tag=0
-        //음식점 임시 주소 tag를 통해 String 객체를 지정할 수 있음
+        //음식점 임시 주소 tag를 통해 객체를 지정할 수 있음
 
-
-        for(i in 1..10) {
-            var temp=LatLng(37.282753,127.044999+(i*0.0005))
+        var k=datas.size
+        for(j in 0..(k-1)) {
+            var temp=LatLng(37.282753,127.044999+(0.0006*(j+1)))
             googleMap.addMarker(
                 MarkerOptions()
                     .position(temp)
-                    .title("check${i}")
-                    .snippet("test restraint $i")
+                    .title("${datas[j].restname}")
+                    .snippet("${datas[j].resttag}")
                     .icon(bitmapDescriptorFromVector(mContext,R.drawable.marker_unclicked))
 
-            )?.tag=i;
+            )?.tag=datas[j];
         }
         //지도 초기 확대.
         googleMap.moveCamera(CameraUpdateFactory.zoomTo(15.7f))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(ajou))
         googleMap.setOnMarkerClickListener (this)
         googleMap.setOnMapClickListener(this)
+
     }
 
     override fun onMarkerClick(p0: Marker) : Boolean {
 
-        cardview.visibility = View.GONE
+        binding.cardView.visibility = View.GONE
         prev_marker?.setIcon(bitmapDescriptorFromVector(mContext,R.drawable.marker_unclicked))
         if(p0.tag==0) {
             Toast.makeText(mContext, "현재 위치입니다.", Toast.LENGTH_SHORT).show()
@@ -110,18 +127,9 @@ class SearchmapFragment : Fragment(), OnMapReadyCallback,GoogleMap.OnMarkerClick
         else {
             //tag에 담긴 data 통해 등장할 cardview내의 정보값을 변경
                 p0.setIcon(bitmapDescriptorFromVector(mContext,R.drawable.marker_clicked))
-            var resimg=rootView.findViewById<ImageView>(R.id.rest_img)
-            var restag=rootView.findViewById<TextView>(R.id.rest_tag)
-            var resadmit=rootView.findViewById<TextView>(R.id.rest_admit)
-            var username=rootView.findViewById<TextView>(R.id.rest_name)
-            var resseat_num=rootView.findViewById<TextView>(R.id.rest_seat_num)
-            var res_reserve_time=rootView.findViewById<TextView>(R.id.reserve_time)
-            restag.setText("테스트 음식점 ${p0.tag}")
-            resadmit.setText("${p0.tag}")
-            resseat_num.setText("12")
-            username.setText("테스트 사람 ${p0.tag}")
-            res_reserve_time.setText("40");
-            cardview.visibility=View.VISIBLE
+
+            binding.searchRecyclerview.adapter=SearchAdapter(mutableListOf<SearchData>(p0.tag as SearchData))
+            binding.cardView.visibility=View.VISIBLE
             prev_marker=p0
         };
 
@@ -142,33 +150,33 @@ class SearchmapFragment : Fragment(), OnMapReadyCallback,GoogleMap.OnMarkerClick
 
         prev_marker?.setIcon(bitmapDescriptorFromVector(mContext,R.drawable.marker_unclicked))
         prev_marker=null;
-        cardview.visibility = View.GONE
+        binding.cardView.visibility = View.GONE
         return
     }
 
 
     override fun onStart() {
         super.onStart()
-        mView.onStart()
+        binding.mapView.onStart()
     }
     override fun onStop() {
         super.onStop()
-        mView.onStop()
+        binding.mapView.onStop()
     }
     override fun onResume() {
         super.onResume()
-        mView.onResume()
+        binding.mapView.onResume()
     }
     override fun onPause() {
         super.onPause()
-        mView.onPause()
+        binding.mapView.onPause()
     }
     override fun onLowMemory() {
         super.onLowMemory()
-        mView.onLowMemory()
+        binding.mapView.onLowMemory()
     }
     override fun onDestroy() {
-        mView.onDestroy()
+        binding.mapView.onDestroy()
         super.onDestroy()
     }
 
